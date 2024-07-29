69b59e1ed6bcb6f6188262aef98e406e98f379d4
lorduzuamki <alemuteddy@gmail.com>
2024-07-29 12:28:40 -0600

    Finished expense tracker

--- NewExpenseCSV.py
+++ NewExpenseCSV.py
@@ -36,7 +36,7 @@ class NewExpenseCSV:
             category = 'No category name is given'
         
         #writing in the file in each row
-        with open(self.file_name, 'w', newline='') as file:
+        with open(self.file_name, 'a', newline='') as file:
             expense = csv.writer(file)
             expense.writerow([date_input, price_amount, description, category])
         print("Expense added successfully!")
@@ -56,7 +56,10 @@ class NewExpenseCSV:
             if not expenses:
                 print("No expenses to show")
             else:
+                #Iterate through row from expenses list
+                #using i as index and each title that are assoiceated with value
                 for i, row in enumerate(expenses, start=1):
+                    # assign each key to a row for loop
                     date_input, price_amount, description, category = row
                     print(f"{i}. Date: {date_input}, Amount: {price_amount}, Description: {description}, Category: {category}")
 
@@ -69,27 +72,52 @@ class NewExpenseCSV:
 
         with open(self.file_name, 'r') as file:
             expenses = list(csv.reader(file))
+            #if user select 1 and that exists in the file
             if 0 < expense_number <= len(expenses):
                 del expenses[expense_number - 1]
+                #then delete that file with minus 1, because list indices are zero-based
                 with open(self.file_name, 'w', newline='') as file:
                     writer = csv.writer(file)
                     writer.writerows(expenses)
                 print("Expense deleted successfully!")
             else:
                 print("Invalid expense number")
-
+    def search_expense(self):
+        if not os.path.exists(self.file_name):
+            print("no expense Exists")
+        
+        #user input on date or catergory or description to find the record
+        expense_search = input("Enter either Date ( mm/dd/yyyy) or category or description in the search ").lower()
+        
+        with open(self.file_name, 'r') as file:
+            search_file = list(csv.reader(file))
+            
+            result = [row for row in search_file if expense_search in row[0].lower() or expense_search in row[2].lower() or expense_search in row[3].lower()]
+            if not result:
+                print("No match expenses found")
+            else:
+                for i, row in enumerate(result, start=1):
+                    date_input, price_amount, description, category = row
+                    print(f"{i}, Date:{date_input}, Amount: {price_amount}, Description:{description}, Catagory: {category}")
+        
     def show_summary(self):
         if not os.path.exists(self.file_name):
             print("No expenses to show")
             return
+        #opens a file from path and checks if there is any file
         with open(self.file_name, 'r') as file:
             file_reader = list(csv.reader(file))
             if not file_reader:
                 print("No expenses to show")
                 return
-
-            category_totals = defaultdict(float)
+            #sets a dictrary of category_totals as empty
+            #so each catergory can fill wtih the total amount
+            category_totals = {}
             total_expense = 0.0
+            #sets Expense time in empty list.
+            #as user input datetime, It starts to add it in the expense time list
+            expense_time = []
+            
             for row in file_reader:
                 date, amount, description, category = row
                 try:
@@ -97,12 +125,27 @@ class NewExpenseCSV:
                 except ValueError:
                     print(f"Invalid amount '{amount}' in the file. Skipping this entry.")
                     continue
-                category_totals[category] += amount
+                #this is for all total expenses rom all the categories
                 total_expense += amount
-
-            print(f"Total expenses: ${total_expense:.2f}")
+                expense_time.append(datetime.strptime(date, '%m/%d/%Y'))
+                
+                #Checking category in category_totals exsits in dictionary
+                    #if category exsits update the amount within the category
+                    #if not just add the new catergory in the dictionary
+                
+                if category in category_totals:
+                
+                    category_totals[category] += amount
+                else:
+                    category_totals[category] = amount
+                
+                
+            
             for category, total in category_totals.items():
                 print(f"{category}: ${total:.2f}")
+                
+            print(f"Total expenses: ${total_expense:.2f}")
+            print(f"Description : {description}")
 
 def main():
     expense_tracker = NewExpenseCSV()
@@ -112,7 +155,8 @@ def main():
         print("2. View expenses")
         print("3. Delete an expense")
         print("4. Show summary")
-        print("5. Exit")
+        print("5. Search")
+        print("6. Exit")
         choice = input("Choose an option: ")
         if choice == '1':
             expense_tracker.add_expenses()
@@ -123,6 +167,8 @@ def main():
         elif choice == '4':
             expense_tracker.show_summary()
         elif choice == '5':
+            expense_tracker.search_expense()
+        elif choice == '6':
             break
         else:
             print("Invalid choice. Please try again.")
